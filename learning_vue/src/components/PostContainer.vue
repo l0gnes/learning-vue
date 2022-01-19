b57f453c605ec824cf662703be1b7ccf10f13ab3
l0gnes <loonargt@gmail.com>
2022-01-19 18:16:35 -0400

    Merge Upstream

---
+++ learning_vue/src/components/PostContainer.vue
@@ -0,0 +1,32 @@
+<template>
+    <table>
+        <tr>
+            <td>
+                <img :src="require(`../assets/${postImage}`)"/>
+            </td>
+            <td>
+                <h1>
+                    {{ postHeader }}
+                </h1>
+                <p>
+                    {{ postContent }}
+                </p>
+            </td>
+        </tr>
+    </table>
+</template>
+
+<script>
+export default {
+	name: 'PostContainer',
+	props: {
+        postHeader : String,
+        postContent : String,
+        postImage : URL
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
